1b935bec32dea92ea03fe2ee968c9c6e4493eb79
Aniluj <julianvolonterio@gmail.com>
2018-10-10 21:50:29 -0300

    Botón de búsqueda agregado
    Se agregó un botón que se encarga de buscar una película en concreto hardcodeada. Esto se hizo principalmente para realizar un testeo de UnityWebrequest.

---
+++ DVM-WebServices-TP1/Assets/Scripts/SearchButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class SearchButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Search()
+    {
+        StartCoroutine(GetText());
+    }
+
+    public IEnumerator GetText()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://www.omdbapi.com/?apikey=81876aef&s=Matrix"))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+}

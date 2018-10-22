1d40e138aa9b3e326b7f70bce79d9cca3a0d5c97
Aniluj <julianvolonterio@gmail.com>
2018-10-22 05:37:44 -0300

    Agregado un Input Field y botón de búsqueda modificado
    Se modificó el tamaño y posición del botón de búsqueda y se agregó una caja de texto, que se utilizará en el futuro para escribir lo que se desee buscar.

--- DVM-WebServices-TP1/Assets/Scripts/SearchButton.cs
+++ DVM-WebServices-TP1/Assets/Scripts/SearchButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class SearchButton : MonoBehaviour {
